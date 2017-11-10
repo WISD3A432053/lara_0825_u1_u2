676773cbaad06440f77b50e54c6faa6afeb49c47
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-11-10 16:21:12 +0800

    開啟 Comment.php ，增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
