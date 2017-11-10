ccbb88d5500d37cf65776f8c2b1daca4b9627621
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-11-10 15:50:33 +0800

    了解 Model 和 Collection 的差異,取得 Model2

--- routes/web.php
+++ routes/web.php
@@ -68,9 +68,12 @@ Route::get('/', function () {
 
     $featuredPosts = \App\Post::where('is_feature', 1)->get();
     dd($featuredPosts);
-    */
+
     $fourthPost = \App\Post::find(4);
     dd($fourthPost);
+    */
+    $lastPost = \App\Post::orderBy('id', 'DESC')->first();
+    dd($lastPost);
 
     return view('welcome');
 });
