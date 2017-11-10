ed401c32dcd9fa0d7d90361428d8f1ce53ff9de2
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-11-10 17:31:11 +0800

    使用 Faker 讓資料更擬真

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Post;
 use Carbon\Carbon;
+use Faker\Factory as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -14,12 +15,16 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         Post::truncate();
-
+        $faker = Faker::create('zh_TW');
         $total = 20;
         foreach(range(1, 20) as $number) {
             Post::create([
+                /*
                 'title' => 'title '.$number,
                 'content' => 'content '.$number,
+                */
+                'title'	=>	$faker->sentence,
+                'content'	=>	$faker->paragraph,
                 'is_feature' => rand(0, 1),
                 'created_at' => Carbon::now()->subDays($total - $number),
                 'updated_at' => Carbon::now()->subDays($total - $number),
